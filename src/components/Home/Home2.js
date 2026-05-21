772707d448bffaaef7a3e9b90cdac0d9558f9813
HackerBree <kagwe.bridgette@gmail.com>
2026-05-21 17:43:41 +1000

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -13,10 +13,9 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I’m a Software Engineer who loves transforming ideas into
-              reliable, scalable products. Over time, I’ve explored several
-              technologies and found my passion in building high-performance
-              systems and intuitive user experiences.
+              In this section, please give a brief explanation of who you are. 
+              We want to get to know the real you, your background, and what drives your passion. 
+              Tell the reader what your proficient in for example.
               <br />
               <br />
               I’m proficient in
@@ -26,7 +25,7 @@ function Home2() {
                   JavaScript, C++, Rust, Node.js, and Java{" "}
                 </b>
               </i>
-              — and I enjoy working across both backend and frontend stacks.
+             then tell us what your main areas of interest are for example.
               <br />
               <br />
               My key areas of interest include developing
@@ -36,10 +35,10 @@ function Home2() {
                   Web Applications, Blockchain Solutions,{" "}
                 </b>
               </i>
-              and exploring new ways to bridge on-chain and off-chain systems.
+              Don't be shy, boost about what your good at, it's not arrogance it's selling yourself.
               <br />
               <br />
-              Whenever possible, I love building projects with
+              I am undeniably good with
               <b className="purple"> Node.js </b> and modern frameworks like{" "}
               <i>
                 <b className="purple">React.js</b> and{" "}
