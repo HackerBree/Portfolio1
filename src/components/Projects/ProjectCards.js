34906ac22e0e2e1ed1676532cc48c29653269c61
Bree Kagwe <kagwe.bridgette@gmail.com>
2026-05-23 14:10:27 +1000

    Update ProjectCards.js

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -5,14 +5,46 @@ import Button from "react-bootstrap/Button";
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      <div style={{
+        width: "100%",
+        height: "220px",
+        overflow: "hidden",
+      }}>
+        <img
+          src={props.imgPath}
+          alt="card-img"
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+          }}
+        />
+      </div>
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
         <div style={{ textAlign: "center" }}>
-          <Button variant="primary" href={props.writeupLink} target="_blank">
+          <Button
+            href={props.writeupLink}
+            target="_blank"
+            style={{
+              backgroundColor: "#c770f0",
+              border: "2px solid #c770f0",
+              color: "white",
+              transition: "all 0.3s ease",
+              fontWeight: "bold",
+            }}
+            onMouseEnter={e => {
+              e.currentTarget.style.backgroundColor = "white";
+              e.currentTarget.style.color = "#c770f0";
+            }}
+            onMouseLeave={e => {
+              e.currentTarget.style.backgroundColor = "#c770f0";
+              e.currentTarget.style.color = "white";
+            }}
+          >
             View Project
           </Button>
         </div>
